bb3fede8cbe58a5b41100870b63ef8c9c4559895
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2021-10-26 12:36:50 +0300

    feat:add Translate clokkkalender

--- momentum/src/scripts/clockcalendar.js
+++ momentum/src/scripts/clockcalendar.js
@@ -1,9 +1,33 @@
 const time = document.querySelector('.time');
-const day = document.querySelector('.date')
+const day = document.querySelector('.date');
+const language = document.querySelector(".language");
+
+console.log(language.checked)
+let langDate = "en-US"
+
+  if (localStorage.getItem("languageValue")=='false') {
+    langDate = "ru-Ru";
+    showDate()
+  }else if(localStorage.getItem("languageValue")=='true'){
+    langDate = "en-US";
+    showDate()
+  }
+
+language.addEventListener('change',()=>{
+  if (localStorage.getItem("languageValue")=='true') {
+    langDate = "en-US"
+    showDate()
+  } else if(localStorage.getItem("languageValue")=='false') {
+    langDate = "ru-Ru";
+    showDate()
+  }
+  
+})
 function showTime() {
   const date = new Date;
-  const dataTime = date.toLocaleTimeString()
-  time.textContent = dataTime;
+  const options = { timeZoneName: 'short' };
+  const dataTime = date.toLocaleTimeString(langDate, options)
+  time.textContent = dataTime.replace('GMT+3', '');
   showDate();
   setTimeout(showTime,1000)
 }
@@ -11,8 +35,9 @@ function showTime() {
 function showDate(){
   const date = new Date;
   const options = {weekday: 'long', month: 'long', day: 'numeric'};
-  const dataDay = date.toLocaleDateString('ru-Ru',options);
+  const dataDay = date.toLocaleDateString(langDate,options);
   day.textContent = dataDay
+  localStorage.setItem("date", dataDay);
 }
 
 export default showTime()
